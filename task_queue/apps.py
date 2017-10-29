7c8af65d2d29eb86a24d2b5a5ddc21248ae6a5b3
ChigChan <ruifei_yu@163.com>
2017-10-29 10:10:23 +0800

    add 1st model

--- task_queue/apps.py
+++ task_queue/apps.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
 
 class TaskQueueConfig(AppConfig):
+    """Task queue Configuration Mdodel"""
     name = 'task_queue'
+
+
+
--- task_queue/models.py
+++ task_queue/models.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
+"""model of the task queue """
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+"""
+    common model for message queue record the actors on the bus stats
+"""
+
+class Worker(models.Model):
+    """model of Worker"""
+    worker_id = models.CharField(max_length=32)
+    adminState = models.IntegerField()
+
+
+class Action(models.Model):
+    """model for actions"""
+    action_id = models.CharField(max_length=32)
+    action_name = models.CharField(max_length=32)
+
+
+class Task(models.Model):
+    """model of Task"""
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=40)
+    reference = models.CharField(max_length=20)
+   
+
+    
